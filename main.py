622a142b7def54660d20a587cbebff72163fde0e
Matheus Penido <matheuspenido.eng@gmail.com>
2023-06-25 17:19:21 -0300

    Showing some attributes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import urllib.request
 import tarfile
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def load_housing_data():
@@ -19,6 +20,11 @@ def main():
     housing = load_housing_data()
     pd.set_option("display.max_columns", None)
     print(housing.head())
+    print(housing.info())
+    print(housing["ocean_proximity"].value_counts())
+    print(housing.describe())
+    housing.hist(bins=50, figsize=(12, 8))
+    plt.show()
 
 
 if __name__ == "__main__":
