68ea43b0d0f3d6f446c66eb0b71a6dc118f63a5e
Matheus Penido <matheuspenido.eng@gmail.com>
2023-07-09 22:49:23 -0300

    Test set understanding

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import urllib.request
 import tarfile
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 
 def load_housing_data():
@@ -18,13 +20,35 @@ def load_housing_data():
 
 def main():
     housing = load_housing_data()
-    pd.set_option("display.max_columns", None)
-    print(housing.head())
-    print(housing.info())
-    print(housing["ocean_proximity"].value_counts())
-    print(housing.describe())
-    housing.hist(bins=50, figsize=(12, 8))
+
+    housing["income_cat"] = pd.cut(housing["median_income"],
+                                     bins=[0., 1.5, 3.0, 4.5, 6.0, np.inf],
+                                     labels=[1, 2, 3, 4, 5])
+
+    strat_train_set, strat_test_set = train_test_split(housing,
+                                                       test_size=0.2,
+                                                       stratify=housing["income_cat"],
+                                                       random_state=42)
+
+    train_set, test_set = train_test_split(housing,
+                                           test_size=0.2,
+                                           random_state=42)
+
+    compare_props = pd.DataFrame({
+        "Overall %": income_cat_proportions(housing),
+        "Stratified %": income_cat_proportions(strat_test_set),
+        "Random %": income_cat_proportions(test_set)
+    }).sort_index()
+    compare_props.index.name = "Income Category"
+    compare_props["Strat. Error %"] = (compare_props["Stratified %"] / compare_props["Overall %"] - 1)
+    compare_props["Rand. Error %"] = (compare_props["Random %"] / compare_props["Overall %"] - 1)
+
+    (compare_props * 100).round(2)
     plt.show()
+
+
+def income_cat_proportions(data):
+    return data["income_cat"].value_counts() / len(data)
 
 
 if __name__ == "__main__":
